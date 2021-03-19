6c70438214024eee6dcf0827ad4679ee0f0d3ab2
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-03-19 12:06:38 +0530

    frontend first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
